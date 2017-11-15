2d8818b0c2877d66efd2e97e80fee6bc0c6a6c3f
RezaMash <rezaa.mash@gmail.com>
2017-11-15 08:37:30 -0800

    Complete the FindPosSupportNew

--- ModelProcessing.py
+++ ModelProcessing.py
@@ -10,6 +10,7 @@ from math import gcd
 from fractions import Fraction
 from Utilities import *
 import multiprocessing
+import qsoptex
 
 zero, one = Fraction(0), Fraction(1)
 
@@ -32,6 +33,7 @@ def reduceMatrix(N, Irr, Filename = 'Reduction.txt'):
     # 6) Find and delete redundant constraints.
 
     # rows[i], cols[i] contains the iteration at which the i-th row, column was deleted
+    print("<> Here!")
     m, n = getSize(N)
     rows, cols = [0]*m, [0]*n
     # currows[i], curcols[i] is the index of the current i-th row, column in original matrix
@@ -414,7 +416,9 @@ def findTBlocked(N, Irrev, basename = 'TBlocked.lp', restricted = True, option =
     Min = -1 if rev else 0
     while len(found) < len(Irrev):
         curName = basename[:index] + str(Iter) + basename[index:]
-        (val, vec) = findPosSupport(N, Irrev, weight, curName, Min = Min, restricted = restricted, option = option)
+        print("FINE")
+        (val, vec) = findPosSupportNew(N, Irrev, weight, curName, Min = Min, restricted = restricted, option = option)
+        print("STILL FINE")
         if val > 0:
             Iter = Iter + 1
             if rev:
@@ -451,15 +455,32 @@ def prepareForCplex(Matrix):
     Matrix = transpose(Matrix)
     return [[Decimal(x.numerator)/Decimal(x.denominator) for x in y] for y in Matrix], Mults
 
+
 def findPosSupportNew(N, support, weight = [1], Filename = 'trial.lp', Min = 0, restricted = True, option = 'row'):
     # This function finds the vector optimizing a given weight in the row/nullspace of N whose
     # support is restricted to a given set of entries; those entries must be non-negative!
     # Note: if the weight vector has a single component, it is automatically taken to be 1!
     # If a nonzero Min value is specified, the components in the support are at least Min.
     # If restricted = False, same but support is NOT restricted to the given set of entries.
+    print("NEW fps!")
     m, n = getSize(N)
+    # intro = 'Problem\n'
     p = qsoptex.ExactProblem()
+    # opt = 'Maximize\n'
     p.set_objective_sense(qsoptex.ObjectiveSense.MAXIMIZE)
+
+
+
+    # if Min:
+    #     if Min > 0:
+    #         bounds += ''.join(['Y' + str(j) + ' >= ' + str(Min) + '\n' for j in support])
+    #     else:
+    #         bounds += ''.join([str(Min) + ' <= Y' + str(j) + ' <= ' + str(-Min) + '\n' for j in support])
+    # else:
+    #     bounds += ''.join(['Y' + str(j) + ' <= 1\n' for j in support])
+    # if Cplex:
+    #     const = const.replace('+ -', '-')
+
     if Min:
         if Min > 0:
             curLower, curUpper = Min, None
@@ -467,6 +488,20 @@ def findPosSupportNew(N, support, weight = [1], Filename = 'trial.lp', Min = 0, 
             curLower, curUpper = Min, -Min
     else:
         curLower, curUpper = 0, 1
+
+    # Cplex is false
+    # if Cplex:
+    #     print()
+
+    # if len(weight) == len(support):
+    #     opt += ' + '.join([str(weight[i]) + ' Y' + str(support[i]) for i in range(len(support)) if weight[i]])
+    # elif len(weight) == 1:   # equal weights, take all of them equal to 1
+    #     opt += ' + '.join(['Y' + str(i) for i in support])
+    # else:
+    #     print('Error: the weight vector is not of the right length!')
+    # opt += '\n'
+    # const = 'Subject' + ' To' * int(Cplex) + '\n'
+
     if len(weight) == len(support):
         for ind, item in enumerate(support):
             p.add_variable(name = 'Y' + str(item), objective = weight[ind], lower = curLower, upper = curUpper)
@@ -475,20 +510,46 @@ def findPosSupportNew(N, support, weight = [1], Filename = 'trial.lp', Min = 0, 
             p.add_variable(name = 'Y' + str(item), objective = 1, lower = curLower, upper = curUpper)
     else:
         print('Error: the weight vector is not of the right length!')
+
+    # if restricted:
+    #     bounds += ''.join(['Y' + str(j) + ' = 0\n' for j in range(n) if j not in support])
     for ind in range(n):
         if ind not in support:
             p.add_variable(name = 'Y' + str(ind), objective = 0, lower = 0, upper = (0 if restricted else None))
-    if option == row:
+
+    # if option == 'row':
+    #     const += ''.join([' + '.join([str(N[i][j]) + ' X' + str(i) for i in range(m) if N[i][j]]) + ' + -1 Y' + str(j) + ' = 0\n' for j in range(n)])
+    # else:
+    #     const += ''.join([' + '.join([str(N[i][j]) + ' Y' + str(j) for j in range(n) if N[i][j]]) + ' = 0\n' for i in range(m)])
+    # bounds = 'Bounds\n'
+    # if option == 'row':
+    #     bounds += ''.join(['X' + str(i) + ' free\n' for i in range(m) if [_f for _f in N[i] if _f]])
+
+    if option == 'row':
         for ind in range(m):
             if [_f for _f in N[ind] if _f]:
                 p.add_variable(name = 'X' + str(ind), objective = 0, lower = None, upper = None)
+        print("THE n IS " + str(n))
         for j in range(n):
             curDict = {'X'+str(i) : N[i][j] for i in range(m)}
             curDict.update({'Y'+str(j) : -1})
             p.add_linear_constraint(qsoptex.ConstraintSense.EQUAL, curDict, rhs = 0)
     else: # option == 'null'
         for i in range(m):
+            print("NEW fps! (17)")
             p.add_linear_constraint(qsoptex.ConstraintSense.EQUAL, {'Y'+str(j) : N[i][j] for j in range(n)}, rhs = 0)
+
+    # opt += 'obj: '
+    # f = open(Filename, 'w')
+    # if not Cplex:
+    #     f.write(intro)
+    # f.write(opt)
+    # f.write(const)
+    # f.write(bounds)
+    # f.write('End\n')
+    # f.close()
+    # return processFile(Filename, True)
+
     return processProblem(p)
 
 def findPosSupport(N, support, weight = [1], Filename = 'trial.lp', Min = 0, restricted = True, Cplex = False, option = 'row'):
@@ -497,6 +558,7 @@ def findPosSupport(N, support, weight = [1], Filename = 'trial.lp', Min = 0, res
     # Note: if the weight vector has a single component, it is automatically taken to be 1!
     # If a nonzero Min value is specified, the components in the support are at least Min.
     # If restricted = False, same but support is NOT restricted to the given set of entries.
+    print("OLD fps!")
     m, n = getSize(N)
     intro = 'Problem\n'
     intro += (Filename.replace('.lp', '') + '\n')
